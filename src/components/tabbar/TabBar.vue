206dc744095c8f788c142087ca2f83f35dc1d59b
fclvhh <374822251@qq.com>
2019-12-08 15:16:53 +0800

    start

---
+++ src/components/tabbar/TabBar.vue
@@ -0,0 +1,27 @@
+<template>
+  <div id="tab-bar">
+    <slot></slot>
+  </div>
+</template>
+
+<script>
+export default {
+  name:"TabBar"
+}
+
+</script>
+<style>
+ #tab-bar {
+    position: fixed;
+    width: 100%;
+    height:49px;
+    bottom: 0px;
+    left: 0px;
+    display:flex;
+    justify-content: space-around;
+    background-color: #f6f6f6;
+    align-items: center;
+    box-shadow: 0px -5px 1px rgba(100,100,100,0.08); 
+  }
+
+</style>
---
+++ src/components/tabbar/TabBarItem.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <slot name=""></slot>
+  </div>
+</template>
+
+<script>
+export default {
+  name:"TabBarItem"
+}
+
+</script>
+<style>
+
+</style>
